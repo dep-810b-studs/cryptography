5cbffc38b4be5c7ccb8a389bd52ecb671b778e3a
Василий Щербаков <v.sherbakov@softpro.com>
2021-02-21 16:09:09 +0300

    add cryptography project

---
+++ Cryptography.Tests/WorkingWithBitsTests.cs
@@ -0,0 +1,63 @@
+using Cryptography.WorkingWithBits;
+using Xunit;
+
+namespace Cryptography.Tests
+{
+    public class WorkingWithBitsTests
+    {
+        [Theory]
+        [InlineData(1,0,1)]
+        [InlineData(0,0,0)]
+        [InlineData(0,10,0)]
+        [InlineData(0b1010,3,1)]
+        [InlineData(0b1010,2,0)]
+        public void IndexerShouldReturnCorrectBit(uint number, int bitNumber, int expectedBit)
+        { 
+            //arrange
+            var text = new OpenedText(number);
+            //act
+            var actualBit = (int) text[bitNumber];
+            //assert
+            Assert.Equal(expectedBit, actualBit);
+        }
+        
+        [Theory]
+        [InlineData(0b1010,3,0)]
+        [InlineData(0b1010,2,1)]
+        [InlineData(0b10101,3,0)]
+        [InlineData(0b10101,3,1)]
+        [InlineData(0b10101,0,0)]
+        [InlineData(0b11111,0,0)]
+        public void IndexerShouldCorrectlySetBit(uint number, int bitNumber, uint bitValue)
+        { 
+            //arrange
+            var text = new OpenedText(number);
+            //act
+            text[bitNumber] = bitValue;
+            var actualBitValue = (int) text[bitNumber];
+            //assert
+            Assert.Equal((int)bitValue, actualBitValue);
+        }
+        
+        
+        [Theory]
+        [InlineData(0b1010,0,1)]
+        [InlineData(0b1010,1,0)]
+        [InlineData(0b10101,3,0)]
+        [InlineData(0b10101,3,1)]
+        public void BitsShouldBeCorrectSwapped(uint number, int firstBitNumber, int secondBitNumber)
+        { 
+            //arrange
+            var text = new OpenedText(number);
+            var expectedFirstBit = text[secondBitNumber]; 
+            var expectedSecondBit = text[firstBitNumber]; 
+            //act
+            text.SwapBits(firstBitNumber, secondBitNumber);            
+            var actualFirstBit = text[firstBitNumber];
+            var actualSecondBit = text[secondBitNumber];
+            //assert
+            Assert.Equal(expectedFirstBit, actualFirstBit);
+            Assert.Equal(expectedSecondBit, actualSecondBit);
+        }
+    }
+}
---
+++ Cryptography.WorkingWithBits/OpenedText.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Cryptography.WorkingWithBits
+{
+    public class OpenedText
+    {
+        private uint _text;
+
+        public OpenedText(uint text)
+        {
+            _text = text;
+        }
+
+        public uint this[int bitNumber]
+        {
+            get => GetIBit(bitNumber);
+            set => SetIBit(bitNumber, value);
+        }
+
+        private uint GetIBit(int bitNumber)
+        {
+            if(bitNumber < 0)
+                throw new ArgumentException(
+                    $"The argument {nameof(bitNumber)} should be correct bit number (less or more then zero) but found {bitNumber}");
+            
+            return _text >> bitNumber & 1;
+        }
+        
+        private void SetIBit(int bitNumber, uint bitValue)
+        {
+            if(bitNumber < 0)
+                throw new ArgumentException(
+                    $"The argument {nameof(bitNumber)} should be correct bit number (less or more then zero) but found {bitNumber}");
+            
+            if (bitValue is not 0 and not 1)
+                throw new ArgumentException(
+                    $"The argument {nameof(bitValue)} should be correct bit value (0 or 1) but found {bitValue}");
+            
+            if(GetIBit(bitNumber) == bitValue)
+                return;
+
+            _text = (~((uint)1 << bitNumber) & _text) | bitValue << bitNumber;
+        }
+
+        public void SwapBits(int firstBitNumber, int secondBitNumber)
+        {
+            var firstBit = GetIBit(firstBitNumber);
+            var secondBit = GetIBit(secondBitNumber);
+            
+            if(firstBit == secondBit)
+                return;
+            
+            SetIBit(secondBitNumber, firstBit);
+            SetIBit(firstBitNumber, secondBit);
+        }
+   }
+}
