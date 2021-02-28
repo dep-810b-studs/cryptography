83c7eef41afebcc6e882d07edf2b79d6a380eb41
sh-vasily <vasia.punk.rok@gmail.com>
2021-02-28 12:39:04 +0300

    sixth task

--- Cryptography.WorkingWithBits/OpenText.cs
+++ Cryptography.WorkingWithBits/OpenText.cs
@@ -82,5 +82,16 @@ namespace Cryptography.WorkingWithBits
             _text = textWithReplacedBits.Value;
             return this;
         }
-   }
+
+        public long FindMaxTwoDegreeThatDivisibleByNumber()
+        {
+            return _multiplyDeBruijnBitPosition2[(((~_text + 1) & _text) * 0x077CB531U) >> 27];
+        }
+
+        private readonly int[] _multiplyDeBruijnBitPosition2 = 
+        {
+            0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
+            31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+        };
+    }
 }
