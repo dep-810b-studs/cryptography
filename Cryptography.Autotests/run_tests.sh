a8aa575e9fb7f8beac4011619a5cb6c3d52d46ce
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-06 21:51:46 +0300

    using release assembly

--- Cryptography.Autotests/run_tests.sh
+++ Cryptography.Autotests/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APP_UNDER_TEST_NAME=Cryptography.DemoApplication
-ASSEMBLY_LOCATION=bin/Debug/net5.0
+ASSEMBLY_LOCATION=bin/Release/net5.0
 APP_UNDER_TEST_BINARIES_DIR=app_under_test_binary
 
 dotnet publish ../$APP_UNDER_TEST_NAME/$APP_UNDER_TEST_NAME.csproj -c Release
