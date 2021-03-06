77158595a7a3559ffb7240116b44166e1d8cadea
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-06 21:30:56 +0300

    cover third test by autotests

--- Cryptography.Autotests/demontraiting_app_tests.py
+++ Cryptography.Autotests/demontraiting_app_tests.py
@@ -1,6 +1,7 @@
 ﻿import sys
 import os
 from subprocess import Popen, PIPE
+import re
 
 if __name__ == "__main__":
 
@@ -16,6 +17,21 @@ if __name__ == "__main__":
 
     system_under_test = Popen(system_under_test_binary_name, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
     print(system_under_test.stdout.readline())
-    system_under_test.stdin.write("1\n")
+    system_under_test.stdin.write("2\n")
     system_under_test.stdin.flush()
     print(system_under_test.stdout.readline())
+    system_under_test.stdin.write("10000000110000001110000011110000\n")
+    system_under_test.stdin.flush()
+    print(system_under_test.stdout.readline())
+    system_under_test.stdin.write("3 2 1 0\n")
+    system_under_test.stdin.flush()
+    expected_applying_permutations_result = "11110000111000001100000010000000"
+    applying_permutations_output = system_under_test.stdout.readline()
+    system_under_test.terminate()
+    actual_applying_permutations_result = re.findall(r'\d+', applying_permutations_output)[0]
+    if actual_applying_permutations_result == expected_applying_permutations_result:
+        print("Test for replacing bytes by permutations passed.")
+    else:
+        print("Test for replacing bytes by permutations not passed.")
+        sys.exit(-1)
+
