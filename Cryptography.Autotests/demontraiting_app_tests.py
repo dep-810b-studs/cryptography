b6f8cd356202a049b5eb32515b04585ab446c5da
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-07 12:51:25 +0300

    [functional-tests] fix error codes

--- Cryptography.Autotests/demontraiting_app_tests.py
+++ Cryptography.Autotests/demontraiting_app_tests.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
 
     if len(sys.argv) < 2:
         print('There is not enough command line arguments. Tests cant work')
-        sys.exit(-1)
+        sys.exit(1)
 
     system_under_test_binary_name = sys.argv[1]
 
     if not os.path.exists(system_under_test_binary_name):
         print(f'File {system_under_test_binary_name} doesnt exist. Program cant work')
-        sys.exit(-1)
+        sys.exit(1)
 
     try:
         system_under_test = SystemUnderTest(system_under_test_binary_name)
@@ -21,10 +21,10 @@ if __name__ == "__main__":
                                                                                    "3 2 1 0")
     except Exception as e:
         print(f"There is something error during tests:{e}. Exiting.")
-        sys.exit(-1)
+        sys.exit(1)
 
     if actual_applying_permutations_result == expected_applying_permutations_result:
         print("Test for replacing bytes by permutations passed.")
     else:
         print("Test for replacing bytes by permutations not passed.")
-        sys.exit(-1)
+        sys.exit(2)
