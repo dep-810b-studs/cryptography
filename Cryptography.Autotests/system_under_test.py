9a34391483c46cb5eabf30cf942b0bb8cafca7de
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-07 13:29:04 +0300

    [functional-tests] remove debug logs

--- Cryptography.Autotests/system_under_test.py
+++ Cryptography.Autotests/system_under_test.py
@@ -4,25 +4,23 @@ from subprocess import PIPE, Popen
 
 class SystemUnderTest:
     def __init__(self, system_under_test_binary_name):
-        print("start sut ctor")
         self.__core = Popen(system_under_test_binary_name, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
-        print("end sut ctor")
 
     def __del__(self):
         if hasattr(self, '__core'):
             self.__core.terminate()
 
     def apply_permutations(self, num, permutations):
-        print(self.__core.stdout.readline())
+        self.__core.stdout.readline()
         self.__core.stdin.write("2\n")
         self.__core.stdin.flush()
-        print(self.__core.stdout.readline())
+        self.__core.stdout.readline()
         self.__core.stdin.write(f"{num}\n")
         self.__core.stdin.flush()
         self.__core.stdout.readline()
         self.__core.stdin.write(f"{permutations}\n")
         self.__core.stdin.flush()
         applying_permutations_output = self.__core.stdout.readline()
-        print(applying_permutations_output)
         actual_applying_permutations_result = re.findall(r'\d+', applying_permutations_output)[0]
         return actual_applying_permutations_result
+
