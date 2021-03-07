50ef31d5f8e48c2a097a78a4ed1ce0dff8b47d42
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-07 13:09:51 +0300

    [functional-tests] temprary advanced logs

--- Cryptography.Autotests/system_under_test.py
+++ Cryptography.Autotests/system_under_test.py
@@ -4,7 +4,9 @@ from subprocess import PIPE, Popen
 
 class SystemUnderTest:
     def __init__(self, system_under_test_binary_name):
+        print("start sut ctor")
         self.__core = Popen(system_under_test_binary_name, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
+        print("end sut ctor")
 
     def __del__(self):
         if hasattr(self, '__core'):
@@ -21,6 +23,6 @@ class SystemUnderTest:
         self.__core.stdin.write(f"{permutations}\n")
         self.__core.stdin.flush()
         applying_permutations_output = self.__core.stdout.readline()
+        print(applying_permutations_output)
         actual_applying_permutations_result = re.findall(r'\d+', applying_permutations_output)[0]
         return actual_applying_permutations_result
-
