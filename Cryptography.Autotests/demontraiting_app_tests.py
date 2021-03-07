260f0a6389847a5076acbf34e72da534707fb829
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-07 12:38:26 +0300

    [functional-tests] using system under test class

--- Cryptography.Autotests/demontraiting_app_tests.py
+++ Cryptography.Autotests/demontraiting_app_tests.py
@@ -1,37 +1,29 @@
 ﻿import sys
 import os
-from subprocess import Popen, PIPE
-import re
+from system_under_test import SystemUnderTest
 
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
         print('There is not enough command line arguments. Tests cant work')
-        sys.exit()
+        sys.exit(-1)
 
     system_under_test_binary_name = sys.argv[1]
 
     if not os.path.exists(system_under_test_binary_name):
         print(f'File {system_under_test_binary_name} doesnt exist. Program cant work')
-        sys.exit()
+        sys.exit(-1)
 
-    system_under_test = Popen(system_under_test_binary_name, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
-    system_under_test.stdout.readline()
-    system_under_test.stdin.write("2\n")
-    system_under_test.stdin.flush()
-    system_under_test.stdout.readline()
-    system_under_test.stdin.write("10000000110000001110000011110000\n")
-    system_under_test.stdin.flush()
-    system_under_test.stdout.readline()
-    system_under_test.stdin.write("3 2 1 0\n")
-    system_under_test.stdin.flush()
-    expected_applying_permutations_result = "11110000111000001100000010000000"
-    applying_permutations_output = system_under_test.stdout.readline()
-    system_under_test.terminate()
-    actual_applying_permutations_result = re.findall(r'\d+', applying_permutations_output)[0]
+    try:
+        system_under_test = SystemUnderTest(system_under_test_binary_name)
+        expected_applying_permutations_result = "11110000111000001100000010000000"
+        actual_applying_permutations_result = system_under_test.apply_permutations("10000000110000001110000011110000", "3 2 1 0")
+    except:
+        print("There is something error during tests, exiting")
+        sys.exit(-1)
+        
     if actual_applying_permutations_result == expected_applying_permutations_result:
         print("Test for replacing bytes by permutations passed.")
     else:
         print("Test for replacing bytes by permutations not passed.")
         sys.exit(-1)
-
