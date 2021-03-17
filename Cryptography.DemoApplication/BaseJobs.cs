3b2862680ad6f0b9ed287ad1a738642ce1fc25b4
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-17 20:01:36 +0300

    add abstraction with logic for all types of jobs

---
+++ Cryptography.DemoApplication/BaseJobs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cryptography.DemoApplication
+{
+    public abstract class BaseJobs : IDemoApplicationJobs
+    {
+        protected delegate void func();
+        protected Delegate[] Actions;
+        public virtual Delegate GetJob(int jobNumber)
+        {
+            if (jobNumber is < 1 or > 5)
+                throw new ArgumentOutOfRangeException($"There is no task with number {jobNumber}");
+
+            return Actions[jobNumber-1];
+        }
+    }
+}
