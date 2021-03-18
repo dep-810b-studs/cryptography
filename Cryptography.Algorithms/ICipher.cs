02b1ed726c81e6c23587ae7f4dd8980f0d2ef0c6
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-18 21:29:43 +0300

    interface for all algorithms

---
+++ Cryptography.Algorithms/ICipher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Cryptography.Algorithms
+{
+    public interface ICipher
+    {
+        byte[] EnCrypt(byte[] openText);
+        byte[] DeCrypt(byte[] encryptedText);
+    }
+}
