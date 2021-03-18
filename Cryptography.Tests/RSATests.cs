e4999677c75733b62351f7207e8761660e9b1d0c
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-18 21:29:01 +0300

    add rsa draft

---
+++ Cryptography.Tests/RSATests.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Cryptography.Algorithms;
+using Xunit;
+
+namespace Cryptography.Tests
+{
+    public class RSATests
+    {
+        private readonly ICipher _cipherUnderTest;
+
+        public RSATests()
+        {
+            _cipherUnderTest = new RSA();
+        }
+
+        [Fact]
+        public void RsaShouldEncryptCorrectly()
+        {
+            //arrange
+            var openText = "hello, world";
+            //act
+            var res = _cipherUnderTest.EnCrypt(Encoding.ASCII.GetBytes(openText));
+            var resInTextFormat = Encoding.ASCII.GetString(res);
+            //assert
+            Assert.Equal(resInTextFormat, openText);
+        }
+    }
+}
