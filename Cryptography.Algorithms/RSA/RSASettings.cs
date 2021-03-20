a320eddd40d3ef59da8f65b4a5e20da74bb1ab1a
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-20 22:35:11 +0300

    add rsa settings for configuring RSA via env args

---
+++ Cryptography.Algorithms/RSA/RSASettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Cryptography.Algorithms.RSA
+{
+    public class RSASettings
+    {
+        public uint PrimeNumberCountBitsMin { get; init; }
+        
+        public uint PrimeNumberCountBitsMax { get; init; }
+    }
+}
