6f044169462d0fd9b34380a136c0f47dfeabc380
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-20 22:36:00 +0300

    add rsa encryption result for usefull work with public, private keys

---
+++ Cryptography.Algorithms/RSA/RSAEncryptionResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Cryptography.Algorithms.RSA
+{
+    public class RSAEncryptionResult
+    {
+        public ulong CipherText { get; init; }
+        public (ulong N, ulong E) PublicKey { get; init; }
+        public (ulong d, uint p, uint q) SecretKey { get; init; }
+    }
+}
