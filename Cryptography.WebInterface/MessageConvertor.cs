c62464b7d9b4b664dd855e01b231e9ba4afbc6ab
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-27 21:10:17 +0300

    implement ui

---
+++ Cryptography.WebInterface/MessageConvertor.cs
@@ -0,0 +1,21 @@
+﻿namespace Cryptography.WebInterface
+{
+    internal interface IMessageConvertor
+    {
+        ulong ConvertToLong(string message);
+        string ConvertToString(ulong message);
+    }
+    
+    internal class MessageConvertor : IMessageConvertor
+    {
+        public ulong ConvertToLong(string message)
+        {
+            return ulong.Parse(message);
+        }
+
+        public string ConvertToString(ulong message)
+        {
+            return message.ToString();
+        }
+    }
+}
---
+++ Cryptography.WebInterface/Rsa/ConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Cryptography.Algorithms.RSA;
+using Cryptography.Arithmetic.ResidueNumberSystem;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cryptography.WebInterface.Rsa
+{
+    public static class ConfigurationExtensions
+    {
+        public static void ConfigureRsa(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<RSASettings>(settings => configuration.GetSection("RSASettings").Bind(settings));
+            services.AddSingleton<IResidueNumberSystem, ResidueNumberSystem>();
+            services.AddSingleton<IRSACipher, RSACipher>();
+            services.AddSingleton<IMessageConvertor, MessageConvertor>();
+            services.AddSingleton<IRsaEncryptionSystem, RsaEncryptionSystem>();
+        }
+    }
+}
