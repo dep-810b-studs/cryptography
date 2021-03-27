754fbd8ba5add3df8a7ef3a876d8ccf3d426fb8a
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-27 21:07:58 +0300

    remove old ui

--- Cryptogaphy.WebInterface/Program.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Cryptogaphy.WebInterface;
-
-Host
-    .CreateDefaultBuilder(args)
-    .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-    .Build()
-    .Run();
