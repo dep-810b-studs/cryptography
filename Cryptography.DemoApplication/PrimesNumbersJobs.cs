7298fd1c4834a710832621a3125251e967976f7e
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-27 17:52:35 +0300

    fixes

--- Cryptography.DemoApplication/PrimesNumbersJobs.cs
+++ Cryptography.DemoApplication/PrimesNumbersJobs.cs
@@ -7,52 +7,126 @@ namespace Cryptography.DemoApplication
     {
         #region Delegates invoking tasks
 
+        /// <summary>
+        /// 1. Напишите программу, выводящую все простые числа, которые меньше m.
+        /// </summary>
         private readonly func _getPrimeNumbersLessThenMJob = () =>
         {
-            int m = GetNumberFromUser();
+            uint m = GetNumberFromUser();
             Console.Write($"Целые простые числа меньшие {m} : ");
-            foreach (var num in ResidueNumberSystem.GetSimpleNumbersLessThenM(m)) Console.Write($"{num} ");
+            var primeNumbers = ResidueNumberSystem.GetSimpleNumbersLessThenM(m);
+            Console.WriteLine(string.Join(" ",primeNumbers));
         };
 
+        /// <summary>
+        /// 2. Выведите на экран приведенную систему вычетов по модулю m.
+        /// </summary>
         private readonly func _getReducedResidueSystemJob = () =>
         {
-            int m = GetNumberFromUser();
+            uint m = GetNumberFromUser();
             var residueNumberSystem = new ResidueNumberSystem()
             {
-                Module = (ulong)m
+                Module = m
             };
-            var arr = residueNumberSystem.ReducedResidueSystem(m);
+            var arr = residueNumberSystem.ReducedResidueSystem();
             Console.Write($"Приведеная система вычетов по модулю {m} : ");
-            foreach (var num in arr) Console.Write($"{num} ");
+            foreach (var num in arr) 
+                Console.Write($"{num} ");
             Console.WriteLine();
         };
         
+        /// <summary>
+        /// 3. Напишите функцию, вычисляющую значение f(m), где
+        /// f(m)−функция Эйлера.
+        /// </summary>
         private readonly func _calculateEylerFunctionJob = () =>
         {
-            int m = GetNumberFromUser();
-            var functionValue = ResidueNumberSystem.CalculateEylerFunction((ulong)m);
+            uint m = GetNumberFromUser();
+            var functionValue = ResidueNumberSystem.CalculateEylerFunction(m);
             Console.WriteLine($" f({m}) = {functionValue}");
         };
+        
+        /// <summary>
+        /// 4. Напишите библиотеку для работы с полной системой вычетов по
+        /// модулю m. Реализуйте в вашей библиотеке арифметические
+        /// операции, операцию возведения в целую степень.
+        /// </summary>
+        private readonly func _workWithFullResidueSystemJob = () =>
+        {
+            var module = GetNumberFromUser("Введите модуль системы:");
+            
+            var residueNumberSystem = new ResidueNumberSystem()
+            {
+                Module = module
+            };
+            
+            while (true)
+            {
+                var firstOperand = GetNumberFromUser("Введите первый операнд:");
+                var secondOperand = GetNumberFromUser("Введите второй операнд:");
+                Console.WriteLine("Введите операцию, которую хотите произвести над числами(+,-,*,/,^)");
+                var operation = Console.ReadLine() ?? throw new ApplicationException("Incorrect input");
+
+                var result = operation switch
+                {
+                    "+" => residueNumberSystem.Add(firstOperand, secondOperand),
+                    "-" => residueNumberSystem.Subtract(firstOperand, secondOperand),
+                    "*" => residueNumberSystem.Multiply(firstOperand, secondOperand),
+                    "/" => residueNumberSystem.Division(firstOperand, secondOperand),
+                    "^" => residueNumberSystem.Pow(firstOperand, secondOperand),
+                    _ => throw new ArgumentOutOfRangeException(nameof(operation),
+                        $"This operation doesnt supported({operation})")
+                };
+                
+                Console.WriteLine($"{firstOperand}{operation}{secondOperand}={result}(mod{residueNumberSystem.Module})");
+                Console.WriteLine($"Для продолжения работы в системе вычетов по модулю {residueNumberSystem.Module} нажмите любую клавишу. Для выхода - q.");
+                var userResponse = Console.ReadLine();
+                if(userResponse is not null and "q")
+                    return;
+            }
+        };  
+        
+        /// <summary>
+        ///5. Реализуйте алгоритм быстрого возведения в степень в кольце вычетов.
+        /// </summary>
+        private readonly func _fastPowAlgorithmJob = () =>
+        {
+            uint m = GetNumberFromUser("Введите модуль кольца вычетов");
+            var residueNumberSystem = new ResidueNumberSystem()
+            {
+                Module =m
+            };
+            
+            uint number = GetNumberFromUser("Введите число, которое хотите возвести в степень");
+            uint degree = GetNumberFromUser("Введите степень");
+
+            var result = residueNumberSystem.Pow(number, degree);
+            
+            Console.WriteLine($"{number}^{degree}={result}(mod {m})");
+        };  
+
         
         #endregion
 
         #region Utils
 
-        private static int GetNumberFromUser()
+        private static uint GetNumberFromUser(string textToUser = null)
         {
-            Console.WriteLine("Введите целое число m : ");
-            return Int32.Parse(Console.ReadLine());
+            Console.WriteLine(textToUser ?? "Введите целое число m :");
+            return UInt32.Parse(Console.ReadLine());
         }
 
         #endregion
 
         public PrimesNumbersJobs()
         {
-            Actions = new func[]
+            Actions = new []
             {
                 _getPrimeNumbersLessThenMJob,
                 _getReducedResidueSystemJob,
-                _calculateEylerFunctionJob
+                _calculateEylerFunctionJob,
+                _workWithFullResidueSystemJob,
+                _fastPowAlgorithmJob
             };
         }
     }
