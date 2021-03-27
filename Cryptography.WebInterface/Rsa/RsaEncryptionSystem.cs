76baee3a321c2977c1f3fc1df58035c88b8b74b3
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-27 21:12:32 +0300

    rsa wrapper for communicating ui and algorithm

---
+++ Cryptography.WebInterface/Rsa/RsaEncryptionSystem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Cryptography.Algorithms.RSA;
+
+namespace Cryptography.WebInterface.Rsa
+{
+    public interface IRsaEncryptionSystem
+    {
+        string Encrypt(string message, string P, string Q, string e);
+        string Decrypt(string message, string N, string D);
+    }
+
+    internal class RsaEncryptionSystem : IRsaEncryptionSystem
+    {
+        private readonly IMessageConvertor _messageConvertor;
+        private readonly IRSACipher _rsaCipher;
+
+        public RsaEncryptionSystem(IMessageConvertor messageConvertor, IRSACipher rsaCipher)
+        {
+            _messageConvertor = messageConvertor;
+            _rsaCipher = rsaCipher;
+        }
+
+        public string Encrypt(string message, string P, string Q, string e)
+        {
+            var convertedMessage = _messageConvertor.ConvertToLong(message);
+            var convertedP = UInt32.Parse(P);
+            var convertedQ = UInt32.Parse(Q);
+            var convertedE = _messageConvertor.ConvertToLong(e);
+            var encryptedMessage = _rsaCipher.EnCrypt(convertedMessage,
+                convertedP, convertedQ, convertedE);
+
+            return _messageConvertor.ConvertToString(encryptedMessage.CipherText);
+        }
+
+        public string Decrypt(string message, string N, string D)
+        {
+            var convertedMessage = _messageConvertor.ConvertToLong(message);
+            var convertedN = UInt32.Parse(N);
+            var convertedD = UInt32.Parse(D);
+
+            var encryptionResult = new RSAEncryptionResult
+            {
+                CipherText = convertedMessage,
+                PublicKey = (convertedN,0),
+                SecretKey = (convertedD, 0, 0)
+            };
+
+            var decryptedMessage = _rsaCipher.DeCrypt(encryptionResult);
+
+            return _messageConvertor.ConvertToString(decryptedMessage);
+        }
+    }
+    
+}
