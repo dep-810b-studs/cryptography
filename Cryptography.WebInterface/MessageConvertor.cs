330ccd1da6cb4587c365577ab69d29e5e669590e
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-28 12:03:06 +0300

    implement message convertor

--- Cryptography.WebInterface/MessageConvertor.cs
+++ Cryptography.WebInterface/MessageConvertor.cs
@@ -1,21 +1,50 @@
-﻿namespace Cryptography.WebInterface
+﻿using System;
+using System.Linq;
+using System.Text;
+using Cryptography.Algorithms;
+
+namespace Cryptography.WebInterface
 {
-    internal interface IMessageConvertor
+    public interface IMessageConvertor
     {
         ulong ConvertToLong(string message);
         string ConvertToString(ulong message);
     }
     
-    internal class MessageConvertor : IMessageConvertor
+    public class MessageConvertor : IMessageConvertor
     {
+        private const uint _maxMessageSizeInBytes = 8;
+        
         public ulong ConvertToLong(string message)
         {
-            return ulong.Parse(message);
+            var bytesCountInMessage = Encoding.UTF8.GetByteCount(message); 
+            
+            if (Encoding.UTF8.GetByteCount(message) > _maxMessageSizeInBytes)
+                throw new ArgumentException("Message have too large size");
+
+            var messageInBytes = Encoding.UTF8.GetBytes(message).ToList(); 
+            
+            if (bytesCountInMessage < 8)
+            {
+                for (int i = 0; i < 8 - bytesCountInMessage; i++)
+                {
+                    messageInBytes = messageInBytes.Prepend((byte)0).ToList();
+                }
+            }
+            
+            return messageInBytes
+                .ToArray()
+                .ToUInt64();
+
+            //return ulong.Parse(message);
         }
 
         public string ConvertToString(ulong message)
         {
-            return message.ToString();
+            var messageInBytes = message.ToByteArray();
+            return Encoding.UTF8.GetString(messageInBytes);
+
+            //return message.ToString();
         }
     }
 }
