c6cc1c086633148e0789174ddc641f56c5725a07
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-30 18:56:16 +0300

    remove not actual test cases

--- Cryptography.Tests/RSATests.cs
+++ Cryptography.Tests/RSATests.cs
@@ -23,18 +23,6 @@ namespace Cryptography.Tests
         }
 
         [Theory]
-        [InlineData(2,7,11,37,13,51)]
-        [InlineData(123,11,13,113,17,41)]
-        public void RsaShouldEncryptCorrectly(ulong message, uint p, uint q, ulong E,ulong d, ulong cipherMessage)
-        {
-            //act
-            var actualCipherResult = _rsaCipher.EnCrypt(message, p, q, E);
-            //assert
-            Assert.Equal(cipherMessage, actualCipherResult.CipherText);
-            Assert.Equal(d,actualCipherResult.SecretKey.d);
-        }
-        
-        [Theory]
         [InlineData(51, 13,7,11,37, 2)]
         public void RsaShouldDecryptCorrectly(ulong cipherMessage, ulong d, ulong p, ulong q, ulong e, ulong expectedMessage)
         {
