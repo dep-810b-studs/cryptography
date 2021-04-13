75fa85af3034de7ef8731b52240c2add70b13602
sh-vasily <vasia.punk.rok@gmail.com>
2021-04-13 21:16:31 +0300

    add test for galois fiels

---
+++ Cryptography.Tests/GaliosFieldTest.cs
@@ -0,0 +1,19 @@
+﻿using Cryptography.Arithmetic.GaloisField;
+using Xunit;
+
+namespace Cryptography.Tests
+{
+    public class GaloisFieldTest
+    {
+        [Theory]
+        [InlineData(123, "x^6 + x^5 + x^4 + x^3 + x + 1")]
+        [InlineData(243, "x^7 + x^6 + x^5 + x^4 + x + 1")]
+        public void ShouldCorrectRepresentInPotentialForm(byte number, string expectedPolynomial)
+        {
+           //act
+           var actualPolynomial = GaloisField.ToPotentialForm(number);
+           //assert
+           Assert.Equal(expectedPolynomial, actualPolynomial);
+        }
+    }
+}
