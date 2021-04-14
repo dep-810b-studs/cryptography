c8cec0d1e613f518ee7c57bc22847a30a03250eb
sh-vasily <vasia.punk.rok@gmail.com>
2021-04-14 08:40:02 +0300

    impliment binary polynomial multiplication

---
+++ Cryptography.Arithmetic/BinaryPolynomial.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cryptography.Arithmetic.WorkingWithBits;
+using Microsoft.VisualBasic;
+
+namespace Cryptography.Arithmetic
+{
+    public class BinaryPolynomial
+    {
+        public uint Value { get; }
+
+        public BinaryPolynomial(uint value)
+        {
+            Value = value;
+        }
+
+        public static BinaryPolynomial operator *(BinaryPolynomial firstPolynomial, BinaryPolynomial secondPolynomial)
+        {
+            int result = 0;
+
+            var a = (int)firstPolynomial.Value;
+            var b = (int)secondPolynomial.Value;
+
+            while (a != 0)
+            {
+                result ^= (a & 1) * b;
+                b <<= 1;
+                a >>= 1;
+            }
+
+            
+            return new BinaryPolynomial((uint)result);
+        }
+
+        public OpenText ToOpenText() => new (Value);
+        
+        public override string ToString() => ToPotentialForm(Value, 32);
+        public static string ToPotentialForm(uint value, int countBitsInNumber)
+        {
+            var numberToConvert = new OpenText(value);
+            return Enumerable
+                .Range(0,countBitsInNumber)
+                .Reverse()
+                .Where(degree => numberToConvert[degree] == 1)
+                .Select(degree => degree switch
+                {
+                    0 => "1",
+                    1 => "x",
+                    _ => $"x^{degree}" 
+                })
+                .Aggregate((prev, next) => $"{prev} + {next}");
+        }
+    }
+}
