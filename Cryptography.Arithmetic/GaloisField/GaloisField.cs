607efe87b5d1e9928c48aff0d8212c148ca6b56d
sh-vasily <vasia.punk.rok@gmail.com>
2021-04-25 17:58:37 +0300

    add fast pow method, default ctor with irreducible polynomial 11b

--- Cryptography.Arithmetic/GaloisField/GaloisField.cs
+++ Cryptography.Arithmetic/GaloisField/GaloisField.cs
@@ -8,14 +8,14 @@ namespace Cryptography.Arithmetic.GaloisField
 {
     public class GaloisField
     {
-        public GaloisField(uint m)
+        public GaloisField(uint m = 0x11b)
         {
             IrreduciblePolynomial = m;
         }
 
         public BinaryPolynomial IrreduciblePolynomial { get; init; }
 
-        public uint Multiply(byte firstNumber, byte secondNumber)
+        public byte Multiply(byte firstNumber, byte secondNumber)
         {
             BinaryPolynomial firstNumberPolynomial = firstNumber;
             BinaryPolynomial secondNumberPolynomial = secondNumber;
@@ -25,7 +25,49 @@ namespace Cryptography.Arithmetic.GaloisField
 
             return multiplicationByModule;
         }
+
         
+        public byte Pow(byte number, byte exponent)
+        {
+            byte result = 1;
+
+            while (exponent != 0)
+            {
+                if ((exponent & 1) == 1)
+                    result = Multiply(result, number);
+                result = Multiply(result, result);
+                exponent >>= 1;
+            }
+            
+            return result;
+        }
+        
+        public byte MultiplicativeInverse(byte number, MultiplicativeInverseCalculationWay way)
+        {
+            if (number == 0)
+                throw new ArgumentOutOfRangeException();
+
+            return way switch
+            {
+                MultiplicativeInverseCalculationWay.ExtendedEuclideanAlgorithm => MultiplicativeInverseUsingExtendedGCD(
+                    number),
+                MultiplicativeInverseCalculationWay.Exponentiation => MultiplicativeInverseUsingExponentiation(number),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+        
+        private byte MultiplicativeInverseUsingExtendedGCD(byte number)
+        {
+            var (_, inverse, _) = BinaryPolynomial.ExtendedEuclideanAlgorithm(number, IrreduciblePolynomial);
+            return inverse;
+        }
+        /// <summary>
+        /// Using equation a^-1=a^254
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        private byte MultiplicativeInverseUsingExponentiation(byte number) => Pow(number, 254);
+
         /// <summary>
         /// GF(256) = x^7 + x^6 + x^5 + x^4 + x^3 + x^2 + x + 1
         /// </summary>
