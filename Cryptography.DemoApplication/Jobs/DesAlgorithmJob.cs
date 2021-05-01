fbb8b4af7ba1e370ac6489447c587dc392bd2b56
sh-vasily <vasia.punk.rok@gmail.com>
2021-05-01 21:12:56 +0300

    add des job

---
+++ Cryptography.DemoApplication/Jobs/DesAlgorithmJob.cs
@@ -0,0 +1,13 @@
+﻿using Cryptography.Algorithms;
+using Cryptography.Algorithms.DES;
+using Cryptography.Algorithms.Symmetric;
+
+namespace Cryptography.DemoApplication.Jobs
+{
+    [Job("des")]
+    public class DesAlgorithmJob : EncryptionAlgorithmJob
+    {
+        protected override CipherBlockSize _cipherBlockSize => CipherBlockSize.Des;
+        public DesAlgorithmJob() : base(new DesCipher()) { }
+    }
+}
