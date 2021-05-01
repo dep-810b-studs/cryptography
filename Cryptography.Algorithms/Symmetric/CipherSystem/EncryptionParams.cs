f5dbb3aef8f43a09105d1062f04e5a914265c601
sh-vasily <vasia.punk.rok@gmail.com>
2021-05-01 20:38:42 +0300

    add encryption params contract

---
+++ Cryptography.Algorithms/Symmetric/CipherSystem/EncryptionParams.cs
@@ -0,0 +1,10 @@
+﻿namespace Cryptography.Algorithms.Symmetric.CipherSystem
+{
+    public record EncryptionParams(CipherAction CipherAction,
+        CipherBlockSize CipherBlockSize,
+        SymmetricCipherMode SymmetricCipherMode,
+        string InputFileName,
+        string OutputFileName,
+        string KeyFileName,
+        string? InitializationVectorFileName = null);
+}
