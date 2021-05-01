4f98031e25e51c12ec532a3de7b3614b355adea5
sh-vasily <vasia.punk.rok@gmail.com>
2021-05-01 21:13:07 +0300

    refactor

--- Cryptography.DemoApplication/Jobs/RijndelAlgorithmJob.cs
+++ Cryptography.DemoApplication/Jobs/RijndelAlgorithmJob.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Cryptography.Algorithms;
 using Cryptography.Algorithms.Rijandel;
 using Cryptography.Algorithms.Symmetric;
 using Cryptography.Algorithms.Symmetric.CipherManager;
@@ -9,61 +10,9 @@ using Cryptography.Algorithms.Symmetric.Padding;
 namespace Cryptography.DemoApplication.Jobs
 {
     [Job("rijndel")]
-    public class RijndelAlgorithmJob : BaseJobs
+    public class RijndelAlgorithmJob : EncryptionAlgorithmJob
     {
-        private readonly ISymmetricSystem _symmetricSystem;
-
-        public RijndelAlgorithmJob()
-        {
-            Actions = new Action[]
-            {
-                Encrypt,
-                Decrypt,
-                GenerateRandomKey
-            };
-            var symmetricCipher = new RijandelCipher();
-            var paddingService = new PaddingService();
-            var symmetricCipherManager = new SymmetricCipherManager(symmetricCipher, paddingService);
-            _symmetricSystem = new SymmetricSystem(symmetricCipherManager);
-        }
-
-        private void Encrypt()
-        {
-            Console.WriteLine("Введите имя файла для шифрования");
-            var inputFileName = Console.ReadLine();
-            Console.WriteLine("Введите имя файла, который нужно сохранить");
-            var outputFileName = Console.ReadLine();
-            Console.WriteLine("Введите имя файла ключа, который нужно сохранить");
-            var keyFileName = Console.ReadLine();
-
-            var encryptionParams = new EncryptionParams(CipherAction.Encrypt, CipherBlockSize.Small,
-                SymmetricCipherMode.ElectronicCodeBook,
-                inputFileName, outputFileName, keyFileName);
-            
-            _symmetricSystem.HandleEncryption(encryptionParams);
-        }
-        
-        private void Decrypt()
-        {
-            Console.WriteLine("Введите имя файла для расшифровки");
-            var inputFileName = Console.ReadLine();
-            Console.WriteLine("Введите имя файла, который нужно сохранить");
-            var outputFileName = Console.ReadLine();
-            Console.WriteLine("Введите имя файла ключа, который нужно сохранить");
-            var keyFileName = Console.ReadLine();
-
-            var encryptionParams = new EncryptionParams(CipherAction.Decrypt, CipherBlockSize.Small,
-                SymmetricCipherMode.ElectronicCodeBook,
-                inputFileName, outputFileName, keyFileName);
-            
-            _symmetricSystem.HandleEncryption(encryptionParams);
-        }
-        
-        private void GenerateRandomKey()
-        {
-            Console.WriteLine("Введите имя файла");
-            var fileName = Console.ReadLine();
-            _symmetricSystem.GenerateAndSaveRandomKeyToFile(fileName, CipherBlockSize.Small);
-        }
+        protected override CipherBlockSize _cipherBlockSize => CipherBlockSize.Small;
+        public RijndelAlgorithmJob() : base(new RijandelCipher()) { }
     }
 }
