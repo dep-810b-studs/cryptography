99a8236827b9633939ce64690f98e890f5005ebb
sh-vasily <vasia.punk.rok@gmail.com>
2021-05-01 20:35:16 +0300

    add info about cipher block size to symmetric cipher abstraction

--- Cryptography.Algorithms/Abstractions/ISymmetricCipher.cs
+++ Cryptography.Algorithms/Abstractions/ISymmetricCipher.cs
@@ -1,7 +1,10 @@
-﻿namespace Cryptography.Algorithms
+﻿using Cryptography.Algorithms.Symmetric;
+
+namespace Cryptography.Algorithms
 {
     public interface ISymmetricCipher
     {
+        CipherBlockSize CipherBlockSize { get; set; }
         byte[] Encrypt(byte[] openText, byte[] key);
         byte[] Decrypt(byte[] encryptedText, byte[] key);
     }
