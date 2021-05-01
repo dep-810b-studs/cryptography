117af2d3c99ea6557e3b8a31447011c933f8baa2
sh-vasily <vasia.punk.rok@gmail.com>
2021-05-01 14:59:37 +0300

    add symmetric cipher manager api

--- Cryptography.Algorithms/Symmetric/SymmetricCipherManager.cs
+++ Cryptography.Algorithms/Symmetric/SymmetricCipherManager.cs
@@ -1,21 +1,79 @@
-﻿namespace Cryptography.Algorithms.Symmetric
+﻿using System;
+
+namespace Cryptography.Algorithms.Symmetric
 {
     internal interface ISymmetricCipherManager
     {
-        byte[] Encrypt(byte[] message, byte[] key, SymmetricCipherMode cipherMode);
-        byte[] Decrypt(byte[] message, byte[] key, SymmetricCipherMode cipherMode);
+        byte[] Encrypt(byte[] message);
+        byte[] Decrypt(byte[] message);
+        
+        byte[] Key { get; set; }
+        byte[] InitializationVector { get; set; }
+        SymmetricCipherMode CipherMode { get; set; }
+        CipherBlockSize CipherBlockSize { get; set; }
     }
 
     internal class SymmetricCipherManager : ISymmetricCipherManager
     {
-        public byte[] Encrypt(byte[] message, byte[] key, SymmetricCipherMode cipherMode)
+        #region Manager Data
+
+        private readonly ISymmetricCipher _symmetricCipher;
+
+        #endregion
+        
+        #region Manager API
+
+        public SymmetricCipherManager(ISymmetricCipher symmetricCipher)
         {
-            throw new System.NotImplementedException();
+            _symmetricCipher = symmetricCipher;
         }
 
-        public byte[] Decrypt(byte[] message, byte[] key, SymmetricCipherMode cipherMode)
+        public byte[] Key { get; set; }
+        public byte[] InitializationVector { get; set; }
+        public SymmetricCipherMode CipherMode { get; set; }
+        public CipherBlockSize CipherBlockSize { get; set; }
+        
+        public byte[] Encrypt(byte[] message)
         {
-            throw new System.NotImplementedException();
+            switch (CipherMode)
+            {
+                case SymmetricCipherMode.ElectronicCodeBook:
+                    break;
+                case SymmetricCipherMode.CipherFeedback:
+                    break;
+                case SymmetricCipherMode.OutputFeedback:
+                    break;
+                case SymmetricCipherMode.CipherBlockChaining:
+                    break;
+            }
+            return _symmetricCipher.Encrypt(message, Key);
         }
+
+        public byte[] Decrypt(byte[] message)
+        {
+            switch (CipherMode)
+            {
+                case SymmetricCipherMode.ElectronicCodeBook:
+                    break;
+                case SymmetricCipherMode.CipherFeedback:
+                    break;
+                case SymmetricCipherMode.OutputFeedback:
+                    break;
+                case SymmetricCipherMode.CipherBlockChaining:
+                    break;
+            }
+            return _symmetricCipher.Decrypt(message, Key);
+        }
+
+        #endregion
+
+        #region Modes
+
+        private byte[] ElectronicCodeBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
