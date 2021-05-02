82d5dbfc19dc9b2cdf9aaf844b4737d61efaaa5e
sh-vasily <vasia.punk.rok@gmail.com>
2021-05-02 16:48:45 +0300

    implement CipherBlockChaining mode

--- Cryptography.Algorithms/Symmetric/CipherStrategy/CipherBlockChainingStrategy.cs
+++ Cryptography.Algorithms/Symmetric/CipherStrategy/CipherBlockChainingStrategy.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cryptography.Algorithms.Utils;
 
 namespace Cryptography.Algorithms.Symmetric.CipherStrategy
 {
@@ -8,12 +11,35 @@ namespace Cryptography.Algorithms.Symmetric.CipherStrategy
         public byte[] InitializationVector { get; set; }
         public List<byte[]> Encrypt(ISymmetricCipher symmetricCipher, List<byte[]> openText)
         {
-            throw new System.NotImplementedException();
+            var c = InitializationVector;
+            var encryptedMessage = new List<byte[]>();
+            
+            foreach (var messageBlock in openText)
+            {
+                var xoredBlock = CipherUtils.XorByteArrays(messageBlock, c);
+                var encryptedBlock = symmetricCipher.Encrypt(xoredBlock);
+                c = encryptedBlock;
+                encryptedMessage.Add(encryptedBlock);
+            }
+
+            return encryptedMessage;
         }
 
         public List<byte[]> Decrypt(ISymmetricCipher symmetricCipher, List<byte[]> cipherText)
         {
-            throw new System.NotImplementedException();
+            var c = InitializationVector;
+            var decryptedMessage = new byte[cipherText.Count][];
+
+            Parallel.For(0, cipherText.Count, 
+                blockNumber => decryptedMessage[blockNumber] = symmetricCipher.Decrypt(cipherText[blockNumber]));
+
+            for (int i = 0; i < decryptedMessage.Length; i++)
+            {
+                decryptedMessage[i] = CipherUtils.XorByteArrays(c, decryptedMessage[i]);
+                c = cipherText[i];
+            }
+
+            return decryptedMessage.ToList();
         }
     }
 }
