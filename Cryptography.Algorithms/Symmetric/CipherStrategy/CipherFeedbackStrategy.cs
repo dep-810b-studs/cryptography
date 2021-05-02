a459266b3fd6d144bf225607f51cce47ce185411
sh-vasily <vasia.punk.rok@gmail.com>
2021-05-02 17:44:10 +0300

    implement CipherFeedBack mode

--- Cryptography.Algorithms/Symmetric/CipherStrategy/CipherFeedbackStrategy.cs
+++ Cryptography.Algorithms/Symmetric/CipherStrategy/CipherFeedbackStrategy.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cryptography.Algorithms.Utils;
 
 namespace Cryptography.Algorithms.Symmetric.CipherStrategy
 {
@@ -8,12 +11,34 @@ namespace Cryptography.Algorithms.Symmetric.CipherStrategy
         public byte[] InitializationVector { get; set; }
         public List<byte[]> Encrypt(ISymmetricCipher symmetricCipher, List<byte[]> openText)
         {
-            throw new System.NotImplementedException();
+            var c = InitializationVector;
+            var encryptedMessage = new List<byte[]>();
+            
+            foreach (var messageBlock in openText)
+            {
+                var encryptedBlock = symmetricCipher.Encrypt(c);
+                var xoredBlock = CipherUtils.XorByteArrays(encryptedBlock, messageBlock);
+                c = xoredBlock;
+                encryptedMessage.Add(xoredBlock);
+            }
+
+            return encryptedMessage;
         }
 
         public List<byte[]> Decrypt(ISymmetricCipher symmetricCipher, List<byte[]> cipherText)
         {
-            throw new System.NotImplementedException();
+            var decryptedMessage = new byte[cipherText.Count][];
+
+            Parallel.For(0, cipherText.Count, 
+                blockNumber =>
+                {
+                    var currentBlock = blockNumber > 0 ? cipherText[blockNumber - 1] : InitializationVector;
+                    var decryptdData = symmetricCipher.Encrypt(currentBlock);
+                    var openText = CipherUtils.XorByteArrays(decryptdData, cipherText[blockNumber]);
+                    decryptedMessage[blockNumber] = openText;
+                });
+
+            return decryptedMessage.ToList();
         }
     }
 }
