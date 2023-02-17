a0ff4c72fe67c03772f1ed8dd636aa3bd73cc210
shcherbakov.vas <Vasily.Shcherbakov@infotecs.ru>
2023-02-17 14:44:20 +0300

    remove useless abstraction

--- Core/Cryptography.Algorithms/Abstractions/IUnsymmetricCipher.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Cryptography.Algorithms;
-
-public interface IUnSymmetricCipher
-{
-    ulong EnCrypt(ulong message, (ulong, ulong, ulong) key);
-    ulong DeCrypt(ulong cipherText, (ulong, ulong) ket);
-}
