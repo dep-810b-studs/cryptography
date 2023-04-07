d97546ec76421de91f1f55e6918a31f10ef8b4ee
shcherbakov.vas <Vasily.Shcherbakov@infotecs.ru>
2023-04-07 13:27:52 +0300

    Тест

--- Tests/Cryptography.IntegrationTests/UnitTest1.cs
+++ Tests/Cryptography.IntegrationTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using Cryptography.Algorithms.Symmetric;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Cryptography.IntegrationTests;
@@ -11,18 +12,37 @@ public class UnitTest1
         await using var application = new WebApplicationFactory<Program>();
         var client = application.CreateClient();
 
-        var stream = new MemoryStream(Encoding.UTF8.GetBytes("Hello,world!"));
+        var random = new Random();
+        var randomKey = new byte[(int)CipherBlockSize.Small / 8];
+        random.NextBytes(randomKey);
+
+        var file = File.OpenRead(Path.Combine("result.txt"));
+        
         using var multipartFormDataContent = new MultipartFormDataContent
         {
-            { new StreamContent(stream), "inputFile", "input.txt" },
-            { new StringContent("1"), "cipherAction" }
+            { new StreamContent(file), "FormFile", "input.txt" },
+            { new StreamContent(new MemoryStream(randomKey)), "KeyFile", "input.txt" },
+            { new StringContent("0"), "CipherAction" },
         };
 
         using var response = await client.PostAsync("/api/upload", multipartFormDataContent);
         await using var fileStream = await response.Content.ReadAsStreamAsync();
+        
+        using var multipartFormDataContent2 = new MultipartFormDataContent
+        {
+            { new StreamContent(fileStream), "FormFile", "input.txt" },
+            { new StreamContent(new MemoryStream(randomKey)), "KeyFile", "input.txt" },
+            { new StringContent("1"), "CipherAction" },
+        };
+        
+        using var response2 = await client.PostAsync("/api/upload", multipartFormDataContent2);
+        await using var fileStream2 = await response2.Content.ReadAsStreamAsync();
         using var resultStream = new MemoryStream();
-        await fileStream.CopyToAsync(resultStream);
+        await fileStream2.CopyToAsync(resultStream);
         var resultFileContent = Encoding.UTF8.GetString(resultStream.ToArray());
-        Assert.Equal("Hello, world!", resultFileContent);
+
+        var originalFileContent = await File.ReadAllTextAsync(Path.Combine("result.txt"));
+        
+        Assert.Equal(originalFileContent, resultFileContent);
     }
 }
